8bbd618f3a12e4bb97522fea0b39144a2d982f9a
ForrestHarrington <forrestmharrington@gmail.com>
2022-05-11 10:31:42 -0600

    update 1

---
+++ src/JsonParser.java
@@ -0,0 +1,18 @@
+//import java.util.*;
+
+import java.io.FileReader;
+import java.util.Iterator;
+import java.util.Map;
+
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.*;
+
+public class JsonParser {
+    
+    JsonParser() {
+
+    }
+
+}
---
+++ src/Product.java
@@ -0,0 +1,60 @@
+//import java.util.*;
+
+public class Product {
+
+    //members of a product are the name, price and quantity, you may change the 
+    //price or amount of an item but you cannot change the name, that would be a different product
+    private final String name;
+    private float cost;
+    private int amount;
+
+    Product(String name, float cost, int amount) {
+        this.name = name;
+        this.cost = cost;
+        this.amount = amount;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj == this) {
+            return true;
+        }
+        else if(! (obj instanceof Product)) {
+            return false;
+        }
+        else {
+            Product other = (Product) obj;
+            return this.name.equals(other.getName());
+        }
+    }
+
+    public void itemTaken() {
+        this.amount -= 1;
+    }
+
+    public void itemsAdded(int added) {
+        this.amount += added;
+    }
+
+    //getters and setters
+    public String getName() {
+        return this.name;
+    }
+
+    public float getCost() {
+        return this.cost;
+    }
+
+    public void setCost(float newCost) {
+        this.cost = newCost;
+    }
+
+    public int getAmount() {
+        return this.amount;
+    }
+
+    public void setAmount(int newAmount) {
+        this.amount = newAmount;
+    }
+
+}
